5a5509cb4726019e48d3d532bccee379643aa6d8
Lautaro Garcia <lautaro.garcia88@gmail.com>
2016-04-08 01:00:24 -0300

    renombrado de recursos y borrado de recurso default

--- src/main/java/com/utn/tacs/tp2016c1g4/marvel_webapp/recursos/Grupo.java
+++
@@ -1,5 +0,0 @@
-package com.utn.tacs.tp2016c1g4.marvel_webapp.recursos;
-
-public class Grupo {
-
-}
---
+++ src/main/java/com/utn/tacs/tp2016c1g4/marvel_webapp/recursos/PerfilResource.java
@@ -0,0 +1,17 @@
+package com.utn.tacs.tp2016c1g4.marvel_webapp.recursos;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+@Path("perfiles")
+public class PerfilResource {
+
+	@GET
+	@Produces("application/json")
+	public Response get(){
+		return Response.status(200).entity("{}").build();
+	}
+	
+}
