616ed03bbb32cf9296e117f967182b824855fb4c
marcodardis <marco.dardis@gmail.com>
2016-06-20 17:38:08 -0300

    h2k2

--- src/main/java/com/utn/tacs/tp2016c1g4/marvel_webapp/hk2/MyApplicationBinder.java
+++ src/main/java/com/utn/tacs/tp2016c1g4/marvel_webapp/hk2/MyApplicationBinder.java
@@ -9,15 +9,12 @@ import com.utn.tacs.tp2016c1g4.marvel_webapp.api.dao.Dao;
 import com.utn.tacs.tp2016c1g4.marvel_webapp.api.dao.filter.FiltroGrupo;
 import com.utn.tacs.tp2016c1g4.marvel_webapp.api.dao.filter.FiltroPerfil;
 import com.utn.tacs.tp2016c1g4.marvel_webapp.api.dao.filter.FiltroPersonaje;
-import com.utn.tacs.tp2016c1g4.marvel_webapp.api.dao.filter.FiltroUsuario;
 import com.utn.tacs.tp2016c1g4.marvel_webapp.api.dao.inmemory.GrupoInMemoryDao;
 import com.utn.tacs.tp2016c1g4.marvel_webapp.api.dao.inmemory.PerfilInMemoryDao;
 import com.utn.tacs.tp2016c1g4.marvel_webapp.api.dao.inmemory.PersonajeInMemoryDao;
-import com.utn.tacs.tp2016c1g4.marvel_webapp.api.dao.inmemory.UsuarioInMemoryDao;
 import com.utn.tacs.tp2016c1g4.marvel_webapp.api.domain.Grupo;
 import com.utn.tacs.tp2016c1g4.marvel_webapp.api.domain.Perfil;
 import com.utn.tacs.tp2016c1g4.marvel_webapp.api.domain.Personaje;
-import com.utn.tacs.tp2016c1g4.marvel_webapp.api.domain.Usuario;
 
 public class MyApplicationBinder extends AbstractBinder {
 	@Override
@@ -27,8 +24,6 @@ public class MyApplicationBinder extends AbstractBinder {
 		bind(PerfilInMemoryDao.class).to(new TypeLiteral<Dao<Perfil, FiltroPerfil>>() {
 		}).in(Singleton.class);;
 		bind(PersonajeInMemoryDao.class).to(new TypeLiteral<Dao<Personaje, FiltroPersonaje>>() {
-		}).in(Singleton.class);;
-		bind(UsuarioInMemoryDao.class).to(new TypeLiteral<Dao<Usuario, FiltroUsuario>>() {
 		}).in(Singleton.class);;
 	}
 }
