3bc771b669d16258c4f55ce1417d49ba7a97e14f
marcodardis <marco.dardis@gmail.com>
2016-06-20 20:31:14 -0300

    Ranking basic commit

---
+++ src/main/java/com/utn/tacs/tp2016c1g4/marvel_webapp/api/recursos/RankingResource.java
@@ -0,0 +1,80 @@
+package com.utn.tacs.tp2016c1g4.marvel_webapp.api.recursos;
+
+import java.util.Properties;
+import java.util.Set;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.core.Response.Status;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.utn.tacs.tp2016c1g4.marvel_webapp.api.dao.Dao;
+import com.utn.tacs.tp2016c1g4.marvel_webapp.api.dao.exception.ManyResultsException;
+import com.utn.tacs.tp2016c1g4.marvel_webapp.api.dao.filter.FiltroPerfil;
+import com.utn.tacs.tp2016c1g4.marvel_webapp.api.domain.Perfil;
+import com.utn.tacs.tp2016c1g4.marvel_webapp.api.response.OperationStatus;
+import com.utn.tacs.tp2016c1g4.marvel_webapp.api.response.ranking.RankingGetResponse;
+
+@Path("/ranking")
+public class RankingResource {
+
+	private static final Logger logger = LogManager.getLogger(PerfilResource.class);
+
+	private Dao<Perfil, FiltroPerfil> perfilDao;
+
+	private Properties params;
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response get() {
+		logger.debug("get invocado");
+
+		FiltroPerfil.Builder filtroBuilder = new FiltroPerfil.Builder();
+		filtroBuilder.clear();
+		//filtros vacios = devuelve todos
+		Set<FiltroPerfil> filtros = filtroBuilder.build();
+		filtros.clear();
+		//rankingGetResponse!!
+		Set<Perfil> perfiles = perfilDao.find(filtros);
+		
+		//TODO: calcular ranking
+		
+		RankingGetResponse.Builder responseBuilder = new RankingGetResponse.Builder();
+		//responseBuilder.setPersonajes(perfiles);//en vez de perfiles va el calculo
+		
+		RankingGetResponse response = responseBuilder.build();
+		return Response.status(200).entity(response).build();
+	}
+
+	@Inject
+	public void setPerfilDao(Dao<Perfil, FiltroPerfil> perfilDao) {
+		this.perfilDao = perfilDao;
+	}
+
+	@Context
+	public void setUriInfo(UriInfo uriInfo) {
+		logger.debug("catcheando uri info en perfiles");
+
+		this.params = new Properties();
+		for (String key : uriInfo.getQueryParameters().keySet()) {
+			this.params.setProperty(key, uriInfo.getQueryParameters().getFirst(key));
+			logger.debug("query param[ " + key + " = " + this.params.getProperty(key) + " ]");
+		}
+	}
+
+	private boolean with(String adicionalRequerido) {
+		String with = params.getProperty("with");
+		return with != null ? with.matches(".*?,?" + adicionalRequerido + ",?.*?") : false;
+	}
+}
---
+++ src/main/java/com/utn/tacs/tp2016c1g4/marvel_webapp/api/response/ranking/RankingGetResponse.java
@@ -0,0 +1,70 @@
+package com.utn.tacs.tp2016c1g4.marvel_webapp.api.response.ranking;
+
+
+import java.util.Set;
+
+import javax.ws.rs.core.Response.Status;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import com.utn.tacs.tp2016c1g4.marvel_webapp.api.response.OperationStatus;
+
+@JsonInclude(Include.NON_NULL)
+public class RankingGetResponse {
+
+	private OperationStatus status;
+	
+	@JsonProperty("personajes")
+	private Set<Long> personajes;
+
+	public OperationStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OperationStatus status) {
+		this.status = status;
+	}
+
+	public Set<Long> getPersonajes(){
+		return this.personajes;
+	}
+	
+	public void setPersonajes(Set<Long> personajes) {
+		this.personajes = personajes;
+	}
+
+	public static class Builder {
+
+		private Set<Long> personajes;
+		
+		private OperationStatus operationStatus;
+
+		public Builder setPersonajes(Set<Long> personajes) {
+			this.personajes = personajes;
+			return this;
+		}
+
+		public Builder setOperationStatus(OperationStatus operationStatus) {
+			this.operationStatus = operationStatus;
+			return this;
+		}
+
+		public RankingGetResponse build() {
+			RankingGetResponse response = new RankingGetResponse();
+
+			if (personajes != null) {
+				response.setPersonajes(personajes);
+			}
+			
+			if (operationStatus == null) {
+				operationStatus = new OperationStatus();
+				operationStatus.setStatusCode(Status.OK);
+			}
+
+			response.setStatus(operationStatus);
+			return response;
+		}
+	}
+}
