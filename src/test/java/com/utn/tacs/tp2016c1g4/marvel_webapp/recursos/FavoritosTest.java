b2af4b92dd72bd6a022f3ebcb7aac13518db5420
Marco Dardis <marco.dardis@gmail.com>
2016-06-27 15:41:19 -0300

    favoritos test

--- src/test/java/com/utn/tacs/tp2016c1g4/marvel_webapp/recursos/FavoritosTest.java
+++ src/test/java/com/utn/tacs/tp2016c1g4/marvel_webapp/recursos/FavoritosTest.java
@@ -5,17 +5,38 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.inject.Inject;
+import javax.inject.Singleton;
 import javax.ws.rs.BadRequestException;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import org.glassfish.hk2.api.ServiceLocator;
+import org.glassfish.hk2.api.TypeLiteral;
+import org.glassfish.hk2.utilities.Binder;
+import org.glassfish.hk2.utilities.ServiceLocatorUtilities;
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.client.ClientResponse;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Before;
 import org.junit.Test;
 
+import com.utn.tacs.tp2016c1g4.marvel_webapp.api.dao.Dao;
+import com.utn.tacs.tp2016c1g4.marvel_webapp.api.dao.filter.FiltroGrupo;
+import com.utn.tacs.tp2016c1g4.marvel_webapp.api.dao.filter.FiltroPerfil;
+import com.utn.tacs.tp2016c1g4.marvel_webapp.api.dao.filter.FiltroPersonaje;
+import com.utn.tacs.tp2016c1g4.marvel_webapp.api.dao.filter.FiltroUsuario;
+import com.utn.tacs.tp2016c1g4.marvel_webapp.api.dao.inmemory.GrupoInMemoryDao;
+import com.utn.tacs.tp2016c1g4.marvel_webapp.api.dao.inmemory.PerfilInMemoryDao;
+import com.utn.tacs.tp2016c1g4.marvel_webapp.api.dao.inmemory.PersonajeInMemoryDao;
+import com.utn.tacs.tp2016c1g4.marvel_webapp.api.dao.inmemory.UsuarioInMemoryDao;
+import com.utn.tacs.tp2016c1g4.marvel_webapp.api.domain.Grupo;
+import com.utn.tacs.tp2016c1g4.marvel_webapp.api.domain.Perfil;
+import com.utn.tacs.tp2016c1g4.marvel_webapp.api.domain.Personaje;
+import com.utn.tacs.tp2016c1g4.marvel_webapp.api.domain.Usuario;
 import com.utn.tacs.tp2016c1g4.marvel_webapp.api.recursos.FavoritoResource;
 import com.utn.tacs.tp2016c1g4.marvel_webapp.api.request.favorito.FavoritoPutRequest;
 import com.utn.tacs.tp2016c1g4.marvel_webapp.api.request.usuario.UsuarioPostRequest;
@@ -23,6 +44,7 @@ import com.utn.tacs.tp2016c1g4.marvel_webapp.api.response.favorito.FavoritoGetRe
 import com.utn.tacs.tp2016c1g4.marvel_webapp.api.response.favorito.FavoritoPutResponse;
 import com.utn.tacs.tp2016c1g4.marvel_webapp.api.response.perfil.PerfilGetResponse;
 import com.utn.tacs.tp2016c1g4.marvel_webapp.external.domain.PersonajeMarvel;
+import com.utn.tacs.tp2016c1g4.marvel_webapp.hk2.InMemoryDaoBinder;
 import com.utn.tacs.tp2016c1g4.marvel_webapp.hk2.MyResourceConfig;
 import com.utn.tacs.tp2016c1g4.marvel_webapp.hk2.MyTestResourceConfig;
 
@@ -30,11 +52,13 @@ import static org.junit.Assert.*;
 
 public class FavoritosTest extends JerseyTest {
 
+	private Dao<Personaje, FiltroPersonaje> personajeDao;
+	
 	@Override
 	protected Application configure() {
 		return new MyTestResourceConfig();
-	}
-
+	}	
+	
 	@Test
 	public void testPut() {
 		Response response = null;
@@ -54,21 +78,31 @@ public class FavoritosTest extends JerseyTest {
 		
 		//usuario no existente
 		response = target("/favoritos/10").request().put(Entity.json(putReq), Response.class);
-		assertEquals(Response.Status.NOT_FOUND, response.getStatus());//esto esta devolviendo 404 siempre
+		assertEquals(Response.Status.NOT_FOUND.getStatusCode(), response.getStatus());//esto esta devolviendo 404 siempre
 		FavoritoPutResponse putResponse = response.readEntity(FavoritoPutResponse.class);
 		assertEquals("no existe el perfil solicitado", putResponse.getStatus().getMessage());
 		
 		//personaje no existente
 		response = target("/favoritos/1").request().put(Entity.json(putReq), Response.class);
 		putResponse = response.readEntity(FavoritoPutResponse.class);
-		assertEquals(Response.Status.NOT_FOUND, response.getStatus());
+		assertEquals(Response.Status.NOT_FOUND.getStatusCode(), response.getStatus());
 		assertEquals("no existe el personaje solicitado", putResponse.getStatus().getMessage());
 		
 		//correcto
+		//agrego personaje al dao de personajes
+		Personaje miPersonaje = new Personaje();
+		miPersonaje.setNombre("Hulk");
+		miPersonaje.setDescripcion("Verde");
+		miPersonaje.setImagen("imagenPath");
+		assertEquals("Hulk", miPersonaje.getNombre());
+		personajeDao.save(miPersonaje);
+		
 		putReq.setIdPersonaje(new Long(1));
 		response = target("/favoritos/1").request().put(Entity.json(putReq), Response.class);
 		putResponse = response.readEntity(FavoritoPutResponse.class);
-		assertEquals(Response.Status.CREATED, response.getStatus());
+		assertEquals("no existe el personaje solicitado", putResponse.getStatus().getMessage());
+		assertEquals(Response.Status.CREATED.getStatusCode(), response.getStatus());
+		
 	}
 
 	@Test
@@ -89,9 +123,10 @@ public class FavoritosTest extends JerseyTest {
 		
 		//TODO: agregar favoritos... que funcione el put
 	}
-
-	@Test
-	public void testAgregarFavoritoSinRequest() {
+	
+	@Inject
+	public void setPersonajeDao(Dao<Personaje, FiltroPersonaje> personajeDao) {
+		this.personajeDao = personajeDao;
 	}
-
+	
 }
