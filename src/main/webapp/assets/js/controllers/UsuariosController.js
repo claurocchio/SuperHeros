267499f267bc7df385b10bda6974118b4d08c712
julibel <julieta.belfiore@gmail.com>
2016-06-29 17:23:19 -0300

    Usuarios con grupos

--- src/main/webapp/assets/js/controllers/UsuariosController.js
+++ src/main/webapp/assets/js/controllers/UsuariosController.js
@@ -1,15 +1,16 @@
 app.controller('UsuariosController', ['$scope', 'UsuariosFactory', function($scope,Usuarios) {
-	  $scope.message = 'Hello from UsuariosController';
-       
+	 
 	  $scope.usuariosList = [];
-	  
-	  
+	  $scope.gruposList = ["grupo1","grupo2","grupo3"];
+	  	  
 	  $scope.seleccionar = function(usuario) {
 		 $scope.usuarioActivo;
-		 console.log(usuario._id);
-		 Usuarios.getUsuarioById(usuario._id)
+		 Usuarios.getUsuarioById(usuario.idPerfil)
 		   .success(function(data) {
-		   console.log(data.perfil)
+			   $('#x').hide();
+			   $('#y').hide();
+			   $('#panel').hide('slow');
+			
 		   $scope.usuarioActivo = data.perfil;
 		   $scope.ultimoAcceso = usuario.lastAccess;
 		   $scope.nombre = $scope.usuarioActivo.username;
@@ -22,9 +23,28 @@ app.controller('UsuariosController', ['$scope', 'UsuariosFactory', function($sco
 	  Usuarios.getUsuarios()
 	     .success(function(data) {
 	    	 $scope.usuariosList = data.usuarios; 
+	    	 $scope.seleccionar( $scope.usuariosList[0]);
 	    });
-	    
-	
-	    
-	}]);
+	  
+	  var grupoAnt;	  
+	  $scope.detalle = function(grupo) {
+		  $scope.x = grupo;
+		  $scope.y = $scope.usuarioActivo.username; 
+		  
+		 if(grupoAnt != grupo)
+		 {
+			 $('#x').hide();
+			 $('#y').hide();
+		   $('#panel').hide('slow');
+		   grupoAnt = grupo;
+		 }
+		 
+		 $('#panel').show('slow');
+		 $('#x').show();
+		 $('#y').show();
+		
+		
+	  }
+
+}]);
 
