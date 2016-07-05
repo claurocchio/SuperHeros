ea7d61c874cd3449080c0b2aff88a1304c088e28
Claudio Rocchio <clau.rocchio@gmail.com>
2016-07-05 02:28:17 -0300

    avances en grupos

--- src/main/webapp/assets/js/app.js
+++ src/main/webapp/assets/js/app.js
@@ -1,152 +1,152 @@
-var app = angular.module('marvelApp', ['ngRoute', 'ngResource']);
-
+var app = angular.module('marvelApp', [ 'ngRoute', 'ngResource' ]);
 
 app.config(function($routeProvider) {
 
-$routeProvider
-  .when('/', {
-    templateUrl : 'views/login.html',
-    controller  : 'LoginController'
-  })
+	$routeProvider.when('/', {
+		templateUrl : 'views/login.html',
+		controller : 'LoginController'
+	})
 
-  .when('/usuarios', {
-    templateUrl : 'views/usuarios.html',
-    controller  : 'UsuariosController'
-  })
+	.when('/usuarios', {
+		templateUrl : 'views/usuarios.html',
+		controller : 'UsuariosController'
+	})
 
-  .when('/catalogo', {
-    templateUrl : 'views/catalogo.html',
-    controller  : 'CatalogoController'
-  })
-  
-    .when('/favoritos', {
-    templateUrl : 'views/favoritos.html',
-    controller  : 'FavoritosController'
-  })
-  
-    .when('/grupos', {
-    templateUrl : 'views/grupos.html',
-    controller  : 'GruposController'
-  })
-  
-    .when('/login', {
-    templateUrl : 'views/login.html',
-    controller  : 'LoginController'
-  })
-  
-   .when('/ranking', {
-    templateUrl : 'views/ranking.html',
-    controller  : 'RankingController'
-  })
-  
-    .when('/home', {
-    templateUrl : 'views/home.html',
-    controller  : 'HomeController'
-  })
+	.when('/catalogo', {
+		templateUrl : 'views/catalogo.html',
+		controller : 'CatalogoController'
+	})
 
+	.when('/favoritos', {
+		templateUrl : 'views/favoritos.html',
+		controller : 'FavoritosController'
+	})
 
-  .otherwise({redirectTo: '/'});
+	.when('/grupos', {
+		templateUrl : 'views/grupos.html',
+		controller : 'GruposController'
+	})
+
+	.when('/login', {
+		templateUrl : 'views/login.html',
+		controller : 'LoginController'
+	})
+
+	.when('/ranking', {
+		templateUrl : 'views/ranking.html',
+		controller : 'RankingController'
+	})
+
+	.when('/home', {
+		templateUrl : 'views/home.html',
+		controller : 'HomeController'
+	})
+
+	.otherwise({
+		redirectTo : '/'
+	});
 });
-
-
 
 var urlPersonajes = 'api/personajes';
 // ENDPOINTS
-app.factory('FavoritosFactory', ['$http', function($http) {
+app.factory('FavoritosFactory', [ '$http', function($http) {
 
-    var urlBase = 'api/favoritos';
-    var FavoritosFactory = {};
+	var urlBase = 'api/favoritos';
+	var FavoritosFactory = {};
 
-    FavoritosFactory.getFavoritos = function(userId) {
-    	console.log("userId en favoritos");
+	FavoritosFactory.getFavoritos = function(userId) {
+		console.log("userId en favoritos");
 		console.log(userId);
-		console.log(urlBase+'/'+userId);
-        return $http.get(urlBase+'/'+userId);
-    };
-    
-    FavoritosFactory.getPersonajesPorPag = function(pag) {
-        return $http.get(urlPersonajes+'?page='+pag+'&limit=7');
-    };
-
-    FavoritosFactory.guardarFavoritos = function(listFavoritos,userId){
-    	return $http.put(urlBase+'/'+userId,listFavoritos);
-    }
-    return FavoritosFactory;
-}]);
-
-app.factory('GruposFactory', ['$http', function($http) {
-
-    var urlBase = 'api/grupos/';
-//    var urlPersonajes = 'api/personajes';
-    var GruposFactory = {};
-
-    GruposFactory.getGrupos = function(id) {
-        return $http.get(urlBase+id);
-    };
-    
-//    GruposFactory.addGrupo = function() {
-//        return $http.put(urlBase);
-//    };
-    
-    GruposFactory.nuevo = function(grupo) {
-    	return $http.post(urlBase, grupo);
-    };
-    
-    GruposFactory.getPersonajesPorPag = function(pag) {
-    	console.log(urlPersonajes+'?page='+pag+'&limit=7');
-        return $http.get(urlPersonajes+'?page='+pag+'&limit=7');
-    };
-
-    return GruposFactory;
-}]);
-
-app.factory('UsuariosFactory', ['$http', function($http) {
-
-    var urlBase = 'api/usuarios';
-    var urlPerfil = 'api/perfiles';
-    var urlLogin = 'api/login';
-    
-    var UsuariosFactory = {};
-
-    UsuariosFactory.getUsuarios = function() {
-        return $http.get(urlBase);
-    };
-    
-    UsuariosFactory.getUsuarioById = function(id) {
-        return $http.get(urlPerfil+'/'+id);
-    };
-
-    UsuariosFactory.guardarUsuario = function (usuario) {
-        return $http.post(urlBase, usuario);
-    };
-    
-    UsuariosFactory.login = function (usuario) {
-    	return $http.post(urlLogin,usuario);
-    };    
-
-    return UsuariosFactory;
-}]);
-
-app.factory('RankingFactory', ['$http', function($http) {
-	var urlBase = 'api/ranking'
-		
-	var RankingFactory = [];
-	
-	RankingFactory.getRanking = function() {
-	        return $http.get(urlBase);
-	 };
-	
-    return RankingFactory;
-}]);
-
-app.factory('CatalogoFactory', ['$http', function($http) {
-			
-	var CatalogoFactory = [];
-		
-	CatalogoFactory.getPersonajesPorPag = function(pag) {
-	    	return $http.get(urlPersonajes+'?page='+pag+'&limit=6');
+		console.log(urlBase + '/' + userId);
+		return $http.get(urlBase + '/' + userId);
 	};
 
-    return CatalogoFactory;
-}]);
+	FavoritosFactory.getPersonajesPorPag = function(pag) {
+		return $http.get(urlPersonajes + '?page=' + pag + '&limit=7');
+	};
 
+	FavoritosFactory.guardarFavoritos = function(listFavoritos, userId) {
+		return $http.put(urlBase + '/' + userId, listFavoritos);
+	}
+	return FavoritosFactory;
+} ]);
+
+app.factory('GruposFactory', [ '$http', function($http) {
+	var urlBase = 'api/grupos/';
+	// var urlPersonajes = 'api/personajes';
+	var GruposFactory = {};
+
+	GruposFactory.getGrupos = function(id) {
+		return $http.get(urlBase + '?idUsuario=' + id);
+	};
+
+	// GruposFactory.addGrupo = function() {
+	// return $http.put(urlBase);
+	// };
+
+	GruposFactory.nuevo = function(idUsuario, grupo) {
+		return $http.post(urlBase, idUsuario, grupo);
+	};
+
+	GruposFactory.getPersonajesPorPag = function(pag) {
+		console.log(urlPersonajes + '?page=' + pag + '&limit=7');
+		return $http.get(urlPersonajes + '?page=' + pag + '&limit=7');
+	};
+
+	GruposFactory.guardarMiembros = function(listMiembros, userId) {
+		console.log();
+		return $http.put(urlBase + '/' + userId, listMiembros);
+	}
+
+	return GruposFactory;
+} ]);
+
+app.factory('UsuariosFactory', [ '$http', function($http) {
+
+	var urlBase = 'api/usuarios';
+	var urlPerfil = 'api/perfiles';
+	var urlLogin = 'api/login';
+
+	var UsuariosFactory = {};
+
+	UsuariosFactory.getUsuarios = function() {
+		return $http.get(urlBase);
+	};
+
+	UsuariosFactory.getUsuarioById = function(id) {
+		return $http.get(urlPerfil + '/' + id);
+	};
+
+	UsuariosFactory.guardarUsuario = function(usuario) {
+		return $http.post(urlBase, usuario);
+	};
+
+	UsuariosFactory.login = function(usuario) {
+		return $http.post(urlLogin, usuario);
+	};
+
+	return UsuariosFactory;
+} ]);
+
+app.factory('RankingFactory', [ '$http', function($http) {
+	var urlBase = 'api/ranking'
+
+	var RankingFactory = [];
+
+	RankingFactory.getRanking = function() {
+		return $http.get(urlBase);
+	};
+
+	return RankingFactory;
+} ]);
+
+app.factory('CatalogoFactory', [ '$http', function($http) {
+
+	var CatalogoFactory = [];
+
+	CatalogoFactory.getPersonajesPorPag = function(pag) {
+		return $http.get(urlPersonajes + '?page=' + pag + '&limit=6');
+	};
+
+	return CatalogoFactory;
+} ]);
--- src/main/webapp/assets/js/controllers/GruposController.js
+++ src/main/webapp/assets/js/controllers/GruposController.js
@@ -1,88 +1,106 @@
 app.controller('GruposController', [ '$scope', 'GruposFactory', function($scope, Grupos) {
 	$scope.message = 'Hello from GruposController';
-	$scope.blackp = false;
-	$scope.wolv = false;
-	$scope.activeGroup = {
-		id : 0,
-		name : '',
-		cant : '',
-		checked : false,
-		clicked : false,
-		personajes : [ '' ]
-	};
 
-	$scope.grupos = [];
-	$personajesList = [];
+	$scope.gruposList = [];
+	$scope.personajesList = [];
+	$scope.personajesMiembros = [];
 	
-    var pag = 0;
-    
+	var pag = 0;
+	USERID = 1;
+
 	$scope.primeraPag = function() {
 		console.log("esta visible?");
 		console.log($("#ant").is(":visible"));
-		
-		if (pag === 0 && $("#ant").is(":visible") )
-		{
-			$("#ant").hide();		
+
+		if (pag === 0 && $("#ant").is(":visible")) {
+			$("#ant").hide();
 		}
-		
-		if (pag >0 && $("#ant").is(":hidden")){
+
+		if (pag > 0 && $("#ant").is(":hidden")) {
 			console.log("entre al else de primeraPag");
 			console.log(pag);
-			$("#ant").show();	
+			$("#ant").show();
 		}
 	};
-	
-	Grupos.getPersonajes().success(function(data) {
-		$scope.personajesList = data.personajes.name;
-	})
-	
-	    $scope.buscarPersonajes = function() {
-    	Favoritos.getPersonajesPorPag(pag)
-    	.success(function(data) {
-    	 console.log(data);
-    	 $scope.personajesList = data.personajes; 
-    	 if(data.personajes.length < 7)
-    	 {
-    		$("#sig").hide();		
-    	 }
-    	 else
-    	 {
-    		$("#sig").show();	 
-    	 }
-    	 
-    	 if(data.personajes.length === 0)
-    	 {
-    		 $("#ant").hide();
-    	 }
-    	 else
-    	 {
-    		 $("#ant").show();
-    	 }
-    	 $scope.primeraPag();
-    })
-    };
-    
-    $scope.buscarPersonajes();
-    
-	Grupos.getGrupos($scope.USERID).success(function(data) {
+
+//	Grupos.buscarPersonajes().success(function(data) {
+//		$scope.personajesList = data.personajes.name;
+//	})
+
+	$scope.buscarPersonajes = function() {
+		Grupos.getPersonajesPorPag(pag).success(function(data) {
+			console.log(data);
+			$scope.personajesList = data.personajes;
+			if (data.personajes.length < 7) {
+				$("#sig").hide();
+			} else {
+				$("#sig").show();
+			}
+
+			if (data.personajes.length === 0) {
+				$("#ant").hide();
+			} else {
+				$("#ant").show();
+			}
+			$scope.primeraPag();
+		})
+	};
+
+	$scope.buscarPersonajes();
+
+	Grupos.getGrupos(USERID).success(function(data) {
 		$scope.gruposList = data.grupos.name;
 	})
 	
 	$scope.newGroup = function() {
-		var grupoNuevo = { 
-				'name':$scope.name,
-				'idUsuario':$scope.USERID,
+		var request = {
+			'name' : $scope.name,
+			'idUsuario' : USERID,
 		};
-		nuevo(grupoNuevo);
-		
+		nuevo(request);
+		getGrupos;
 	};
-	var nuevo = function (grupo){
+	var nuevo = function(grupo) {
 		Grupos.nuevo(grupo).success(function() {
-			$scope.grupos.push(grupoNuevo);
+			$scope.grupos.push(grupo);
 		})
 	}
 
-
+    $scope.pushear = function() {
+		$scope.personajesList.forEach(function(personaje){
+			console.log($scope.personajesMiembros.indexOf(personaje) === -1);
+			if ($scope.personajesMiembros.indexOf(personaje) === -1){				
+			personaje.checked ? $scope.personajesMiembros.push(personaje.nombre) : null ;
+			}
+		});
+		$scope.guardarMiembros();
+	};   
+	
+    $scope.guardarMiembros = function() {
+    	
+		console.log("personajesList a json");
+		console.log($scope.personajesMiembros);
+		
+    	var request = { nombresPersonaje : $scope.personajesMiembros};
+    	
+    	console.log("va el json!");
+    	console.log(request);
+    	    	
+    	Grupos.guardarMiembros(request,USERID)
+    	.success(function(data) {
+    		 console.log(data);
+    	})
+    	.error(function(data){
+    		 console.log(data);
+			 alert(data.status.message);
+		 });
+    }
+    
+	$scope.eliminar = function(personaje) {
+		var index = $scope.personajesMiembros.indexOf(personaje);
+		  $scope.personajesMiembros.splice(index, 1); 
+		  $scope.guardarMiembros();
+	};
 	/*
 	 * $scope.addChar = function() { angular.forEach($scope.data,
 	 * function(value, key){ if(value.id == activeId){ if(capi == true) {
@@ -93,47 +111,45 @@ app.controller('GruposController', [ '$scope', 'GruposFactory', function($scope,
 	 * });
 	 */
 
-	$scope.assign = function(myId) {
-		activeId = myId;
-	};
+//	$scope.assign = function(myId) {
+//		activeId = myId;
+//	};
+//
+//	$scope.chckedIndexs = [];
+//
+//	$scope.checkedIndex = function(group) {
+//		if ($scope.chckedIndexs.indexOf(group) === -1) {
+//			$scope.chckedIndexs.push(group);
+//		} else {
+//			$scope.chckedIndexs.splice($scope.chckedIndexs.indexOf(group), 1);
+//		}
+//	}
+//
+//	$scope.selectedGroups = function() {
+//		return $filter('filter')($scope.students, {
+//			checked : true
+//		});
+//	};
+//
+//	$scope.remove = function(index) {
+//		angular.forEach($scope.chckedIndexs, function(value, index) {
+//			var index = $scope.data.indexOf(value);
+//			$scope.data.splice($scope.data.indexOf(value), 1);
+//		});
+//		$scope.chckedIndexs = [];
+//	};
 
-	$scope.chckedIndexs = [];
-
-	$scope.checkedIndex = function(group) {
-		if ($scope.chckedIndexs.indexOf(group) === -1) {
-			$scope.chckedIndexs.push(group);
-		} else {
-			$scope.chckedIndexs.splice($scope.chckedIndexs.indexOf(group), 1);
-		}
-	}
-
-	$scope.selectedGroups = function() {
-		return $filter('filter')($scope.students, {
-			checked : true
-		});
-	};
-
-	$scope.remove = function(index) {
-		angular.forEach($scope.chckedIndexs, function(value, index) {
-			var index = $scope.data.indexOf(value);
-			$scope.data.splice($scope.data.indexOf(value), 1);
-		});
-		$scope.chckedIndexs = [];
-	};
-
-	$scope.ant = function(){
-		
+	$scope.ant = function() {
 		pag--;
 		$scope.primeraPag();
-		
+
 		$scope.buscarPersonajes();
 	};
-	
-	$scope.sig = function(){
-		
+
+	$scope.sig = function() {
 		pag++;
 		$scope.primeraPag();
-		
+
 		$scope.buscarPersonajes();
 	};
 } ]);
